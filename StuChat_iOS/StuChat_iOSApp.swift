d3270717b7d72bb39054100f27915f9e6d74b639
OBBRATEN <arturgaragula65@gmail.com>
2023-11-21 00:44:43 +0300

    Initial Commit

---
+++ StuChat_iOS/StuChat_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  StuChat_iOSApp.swift
+//  StuChat_iOS
+//
+//  Created by Arthur Garagulya on 21.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct StuChat_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
